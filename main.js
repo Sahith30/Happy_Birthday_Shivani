86bf7309d4ffeb65e8338ba711a46df60edefe5e
Sahith30 <102732908+Sahith30@users.noreply.github.com>
2022-05-17 20:10:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+function function1() {
+    document.getElementById("song1").play();
+}
+
+function function2() {
+    document.getElementById("song2").play();
+}
+
+function function3() {
+    document.getElementById("song3").play();
+}
+
+function function4() {
+    document.getElementById("song4").play();
+}
+
+function function5() {
+    document.getElementById("song5").play();
+}
+
+function function6() {
+    document.getElementById("song6").play();
+}
+
+function function7() {
+    document.getElementById("song7").play();
+}
+function change_color1() {
+    document.getElementById("anarghya").style.display="inline-block";
+}
+function change_color2() {
+    document.getElementById("ananya").style.display="inline-block";
+}
+function change_color3() {
+    document.getElementById("shivansh").style.display="inline-block";
+}
+function change_color4() {
+    document.getElementById("sahith").style.display="inline-block";
+}
+function change_color5() {
+    document.getElementById("sahana").style.display="inline-block";
+}
+function change_color6() {
+    document.getElementById("aditi").style.display="inline-block";
+}
